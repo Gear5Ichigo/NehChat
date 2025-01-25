b3518aca9c8013026a159d4c4e6c071e9e8ee0b0
zenerz <149389919+Gear5Ichigo@users.noreply.github.com>
2025-01-24 19:51:11 -0500

    s

--- client/src/arcade/pizza/js/main.js
+++ client/src/arcade/pizza/js/main.js
@@ -1,7 +1,7 @@
-import { Application, Graphics } from 'pixi.js';
+import { Application, Container, Graphics } from 'pixi.js';
+import { Button, FancyButton, Input } from '@pixi/ui';
 import { io } from 'socket.io-client';
 import Controls from '../../$repeatedJS/controls';
-import UiButton from '../../$repeatedJS/uibutton';
 
 (async () => {
 
@@ -26,18 +26,35 @@ import UiButton from '../../$repeatedJS/uibutton';
     document.body.style.margin = 0;
     document.body.appendChild(app.canvas)
 
-    //
-    const playbutton = new UiButton({
-        text: "Play!"
-    })
-    playbutton.on("pointerdown", e => {
-        console.log("playing")
-    })
-    //
+    //==
 
-    //
-    app.stage.addChild(playbutton)
-    //
+    const MainMenu = new Container();
+    const GameScreen = new Container();
+
+    const StartMenu = new Container();
+    const LobbyMenu = new Container();
+
+    MainMenu.addChild(StartMenu, LobbyMenu);
+
+    const roomCodeLine = new Input({
+        bg: new Graphics()
+                .rect(0, 0, 250, 80)
+                .fill(0x000000)
+                .stroke({fill: 0xffffff, width: 2}),
+        placeholder: "Enter Room Code",
+        maxLength: 6,
+        align: "center",
+    });
+
+    const joinRoom = new FancyButton({
+
+    });
+    joinRoom.ondo
+
+    StartMenu.add(roomCodeLine, joinRoom);
+    app.stage.add(MainMenu, GameScreen)
+
+    //==
 
     app.ticker.add(ticker => {
         if (controls.getKey("KeyA")) {
